13e231e819b2b8e372dd6c8cbc283164e076a45a
shivakumar <shivakumar.drive1@gmail.com>
2023-08-24 19:23:22 +0530

    asdfkjajslkdjf

--- src/shiva_js/searchBarEventListener.js
+++ src/shiva_js/searchBarEventListener.js
@@ -1,11 +1,11 @@
-
+const listItems = []
 
 export function addSearchBarEventListener() {
 
     const searchBar = document.getElementById("search-dropdown");
     console.log(searchBar);
 
-    const processTypeAheadRequestDebounced = debounce(processTypeAheadRequest, 250); 
+    const processTypeAheadRequestDebounced = debounce(processTypeAheadRequest, 250);
 
     searchBar.addEventListener('input', e => processTypeAheadRequestDebounced(e))
 }
@@ -19,7 +19,7 @@ const processTypeAheadRequest = (e) => {
     fetch(typeaheadUrl).
         then(response => response.json()).
         then(response => {
-          updateDataList(e, response)
+            updateDataList(e, response)
         }); // <- this response will contain the typeahead results
 }
 
@@ -27,36 +27,99 @@ export const updateDataList = (event, response) => {
     const datalist = document.getElementById("search-datalist");
     console.log(datalist);
 
-	// first clear all the elements in the datalist
-	while(datalist.firstChild) {
-		datalist.removeChild(datalist.lastChild)
-	}
+    // first clear all the elements in the datalist
+    while (datalist.firstChild) {
+        datalist.removeChild(datalist.lastChild)
+    }
 
-	// iterating through the results and adding to the datalist
-	response.forEach(elem => {
-		const datalistElem = getDataListElem(elem);
-		datalist.appendChild(datalistElem);
-	});
+    // iterating through the results and adding to the datalist
+    response.forEach(elem => {
+        const datalistElem = getDataListElem(elem);
+        datalist.appendChild(datalistElem);
+    });
 }
 
 const getDataListElem = (elem) => {
-	const optionTemplate = document.getElementById("searchOptionTemplate");
-	console.log(optionTemplate)
-	const optionFragment = optionTemplate.content.cloneNode(true);
-	console.log(optionFragment)
+    const optionTemplate = document.getElementById("searchOptionTemplate");
+    console.log(optionTemplate)
+    const optionFragment = optionTemplate.content.cloneNode(true);
+    console.log(optionFragment)
     // const option = optionFragment.querySelector('searchOption');
-	const option = optionFragment.firstElementChild;
-	console.log(option)
-	const value = elem["stock"]["name"] + " (" +  elem["stock"]["ticker"] + ")";
+    const option = optionFragment.firstElementChild;
+    console.log(option)
+    const value = elem["stock"]["name"] + " (" + elem["stock"]["ticker"] + ")";
     option.value = value
 
     return option
 }
 
-function debounce(func, timeout = 300){
+function debounce(func, timeout = 300) {
     let timer;
     return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
-  }
+}
+
+function updateCard(query) {
+
+    const resultCard = document.getElementById('resultCard');
+
+    const typeaheadUrl = "http://localhost:8083/stocks/typeAhead?q=" + query;
+
+    // use the response from here to render type ahead requests
+    fetch(typeaheadUrl).
+        then(response => response.json()).
+        then(response => {
+            if (response.length > 0) {
+                response.forEach((elem, index) => {
+                    const item = elem["stock"]["name"] + " (" + elem["stock"]["ticker"] + ")";
+
+                    const itemElement = document.createElement('div');
+                    itemElement.textContent = item;
+                    itemElement.classList.add('cursor-pointer', 'hover:text-blue-500', 'px-2', 'py-1');
+
+                    itemElement.addEventListener('click', () => {
+                        selectOption(index);
+                    });
+
+                    listItems.push(itemElement); // Add the item to the collection
+                    resultCard.appendChild(itemElement);
+                })
+
+                // Update selected option index when the list is filtered
+                if (selectedOptionIndex >= 0) {
+                    selectedOptionIndex = filteredData.findIndex(item => item.name === staticData[selectedOptionIndex].name);
+                }
+
+                resultCard.classList.remove('invisible');
+            }
+            else {
+                resultCard.innerHTML = 'No results found.';
+                resultCard.classList.add('invisible');
+            }
+        });
+}
+
+// Event listener for input changes
+// searchInput.addEventListener('input', () => {
+//     const query = searchInput.value;
+//     if (query.length > 0) {
+//         updateCard(query);
+//     } else {
+//         resultCard.innerHTML = ''; // Clear the card when the input is empty
+//         resultCard.classList.add('invisible');
+//     }
+// });
+
+const processTypeAheadRequest2 = (e) => {
+    const resultCard = document.getElementById('resultCard');
+
+    const query = e.target.value;
+    if (query.length > 0) {
+        updateCard(query);
+    } else {
+        resultCard.innerHTML = ''; // Clear the card when the input is empty
+        resultCard.classList.add('invisible');
+    }
+}
